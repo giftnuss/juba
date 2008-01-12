4b5fd92b7359921ca6b36f0ff7de9d0ba1a6cd2e
sk@computer-leipzig.com <sk@computer-leipzig.com@705b867d-5442-0410-b188-5b7b47c1c183>
2008-01-12 14:39:23 +0000

    the first test
    git-svn-id: http://juba.googlecode.com/svn@4 705b867d-5442-0410-b188-5b7b47c1c183

---
+++ trunk/t/001_juba_application.t
@@ -0,0 +1,8 @@
+  use t::Test::Juba
+; use Test::More tests => 1
+
+; BEGIN
+  { use_ok 'Juba::Application'
+  }
+  
+  
---
+++ trunk/t/Test/Juba.pm
@@ -0,0 +1,32 @@
+  package Test::Juba
+# ******************
+; our $VERSION='0.01'
+# *******************
+  
+; use strict; use warnings; use utf8
+  
+; sub import
+  { local $_
+  ; $_->import for qw/strict warnings utf8/ 
+  }
+
+; 1
+  
+__END__
+  
+=head1 NAME
+
+Test::Juba
+  
+=head1 SYNOPSIS
+
+    # first code line in your test file   
+    use t::Test::Juba
+  
+=head1 DESCRIPTION
+
+This modul provides a central point for customization of the testing
+environment.
+  
+It exports the pragmas C<strict>, C<warnings> and C<utf8> in the testfile.
+  
